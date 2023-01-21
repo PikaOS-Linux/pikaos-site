574d9f57498850febbf5032df1bedef79a1ac31e
ferrreo <harderthanfire@gmail.com>
2023-01-21 15:35:15 +0000

    dark by default, fix image loading for the logos, remove unused analytics

--- src/config.mjs
+++ src/config.mjs
@@ -11,7 +11,7 @@ const CONFIG = {
     description: 'PikaOS is a gaming focused linux distribution focussing on ease of use and high compatability. Using the know how from Nobara combined with a Ubuntu base, PikaOS is almost unrivaled in software comaptability.',
     defaultImage: defaultImage,
 
-    defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
+    defaultTheme: 'dark', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
 
     language: 'en',
     textDirection: 'ltr',
@@ -23,7 +23,7 @@ const CONFIG = {
         timeZone: 'UTC',
     }),
 
-    googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+    googleAnalyticsId: false,
     googleSiteVerificationId: false,
 
 };
